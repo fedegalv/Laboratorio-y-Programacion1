9f0611745cd05596dbada0b78e3d41938012d061
Federico <fedegv.4991@gmail.com>
2018-08-31 12:12:36 -0300

    Agregado ejemplos varios

--- Ejemplo_ validacion/UTN_INPUT.h
+++ Ejemplo_ validacion/UTN_INPUT.h
@@ -1,2 +1,20 @@
+
+ /** \brief Permite el ingreso de un entero
+  *
+  * \param [] char mensaje que se mostrara
+  * \param int minimo del rango de validacion
+  * \param int maximo del rango de validacion
+  * \return int el numero validado
+  *
+  */
  int pedirEntero(char [], int, int);
+ /** \brief Valida si el entero esta entre el rango deseado, en este caso 18 y 65
+  *
+  * \param int numero a validar
+  * \param [] char mensaje(en este caso "reingrese", dando a entender que hubo un error"
+  * \param int minimo del rango de validacion
+  * \param int maximo del rango de validacion
+  * \return int el numero validado
+  *
+  */
  int validarEntero(int,char [], int,int);
