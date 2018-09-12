601aea6b2c41859192fc610a422cf88b70599661
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-09-12 16:03:02 -0300

    SORTED BY FOLDERS

--- TP_1 V4/TP_1/OPERACIONES_MATEMATICAS.h
+++
@@ -1,5 +0,0 @@
-float operacionSuma(float, float);
-float operacionResta(float, float);
-float operacionMultiplicacion(float,float);
-float operacionDivision(float,float);
-void operacionFactorial(int,int,unsigned int*,unsigned int*);
