5cbe2a3e7a6c371adc8df3bb2b7ebf06cb6fed37
Federico Galván <40763749+fedegalv@users.noreply.github.com>
2018-09-12 15:58:19 -0300

    mejorado main y agregado de funciones

--- Ej_STRING_NOMBRE_APELLIDO/FUNCIONES.h
+++ Ej_STRING_NOMBRE_APELLIDO/FUNCIONES.h
@@ -1,2 +1,4 @@
 void ingresoNombre(char *);
 void ingresoApellido(char *);
+void upperPrimeraLetra(char *);
+void formateoNombreApellido(char* ,char *,char *);
--- Ej_STRING_NOMBRE_APELLIDO/main.c
+++ Ej_STRING_NOMBRE_APELLIDO/main.c
@@ -7,18 +7,13 @@
 int main()
 {
     char nombre[20];
-    int largo;
     char apellido[20];
     char nombreApellido[20];
     ingresoNombre(nombre);
     ingresoApellido(apellido);
-    largo= strlen(nombre);
-    printf("nombre es %s y apellido es %s, y el largo del nombre es %d\n",nombre,apellido,largo);
-    nombre[0]=toupper(nombre[0]);
-    apellido[0]=toupper(apellido[0]);
-    strcpy(nombreApellido,apellido);
-    strcat(nombreApellido,", ");
-    strcat(nombreApellido, nombre);
+    upperPrimeraLetra(nombre);
+    upperPrimeraLetra(apellido);
+    formateoNombreApellido(nombreApellido,nombre,apellido);
     puts(nombreApellido);
 
     return 0;
